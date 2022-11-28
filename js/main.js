0db439d6e102e04226611aa77b25995791a4d156
your_username <michaelras_@live.com>
2022-11-28 13:54:50 +0100

    whackamole files setup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import RockPaperScissors from "./modules/RockPaperScissors.js";
 import MemoryGame from "./modules/Memorygame.js";
+import Whackamole from "./modules/Whackamole.js";
 
 RockPaperScissors();
 MemoryGame();
+Whackamole();
---
+++ js/modules/Whackamole.js
@@ -0,0 +1,3 @@
+export default function Whackamole() {
+	console.log('hei');
+}
